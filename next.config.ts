b3b3083804060763090c98248c36800e07383de3
eric-euphoria <eric.ncube@euphoria.co.za>
2025-11-04 10:42:22 +0200

    feat(config): Add image domains and remote patterns for improved image handling

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['cdn.pixabay.com'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -8,7 +9,15 @@ const nextConfig = {
         port: '',
         pathname: '/photo/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        pathname: '**',
+      },
     ],
+  },
+  experimental: {
+    optimizeCss: false,
   },
 }
 
